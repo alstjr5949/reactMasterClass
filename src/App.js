e2094e575ade99a6d43651fe88ffdfd00a50a446
alstjr5949 <alstjr5949@gmail.com>
2022-01-07 18:34:28 +0900

    styled component study

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotateAnimation} 2s linear infinite;
+`;
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Wrapper>
+      <Box />
+    </Wrapper>
+  );
 }
 
 export default App;
