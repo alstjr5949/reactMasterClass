9a8d0019affc48cd064ee198b9dbd91b60487828
alstjr5949 <alstjr5949@gmail.com>
2022-01-15 17:59:52 +0900

    coin tracker

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -5,6 +5,7 @@ import Price from "./Price";
 import Chart from "./Chart";
 import { fetchCoinInfo, fetchCoinTickers } from "../api";
 import { useQuery } from "react-query";
+import Helmet from "react-helmet";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -148,11 +149,19 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickerData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId ? coinId : "")
+    () => fetchCoinTickers(coinId ? coinId : ""),
+    {
+      refetchInterval: 5000,
+    }
   );
   const loading = infoLoading || tickersLoading;
   return (
     <Container>
+      <Helmet>
+        <title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </title>
+      </Helmet>
       <Header>
         <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -172,8 +181,8 @@ function Coin() {
               <span>{infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoData?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>$ {tickerData?.quotes.USD.price.toFixed(2)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
