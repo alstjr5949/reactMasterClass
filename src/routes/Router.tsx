564ae3f77c0ed51779d4c71be8e0d4786d414235
alstjr5949 <alstjr5949@gmail.com>
2022-01-23 22:26:11 +0900

    recoil

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Coins from "./Coins";
 import Coin from "./Coin";
 
-function Router() {
+interface IRouterProps {
+  toggleDark: () => void;
+}
+
+function Router({ toggleDark }: IRouterProps) {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/:coinId/*" element={<Coin />}></Route>
-        <Route path="/" element={<Coins />}></Route>
+        <Route path="/" element={<Coins toggleDark={toggleDark} />}></Route>
       </Routes>
     </BrowserRouter>
   );
