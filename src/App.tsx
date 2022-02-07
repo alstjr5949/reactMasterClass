74131c340e7d35cc2b44ca78ec3c756ec45dae43
alstjr5949 <alstjr5949@gmail.com>
2022-02-07 20:19:56 +0900

    basic animaiton ~7.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -65,11 +66,11 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -77,9 +78,12 @@ function App() {
   return (
     <>
       <GlobalStyle />
-
       <Wrapper>
-        <Box />
+        <Box
+          transition={{ type: "spring", delay: 0.5 }}
+          initial={{ scale: 0 }}
+          animate={{ scale: 1, rotateZ: "360deg" }}
+        />
       </Wrapper>
     </>
   );
