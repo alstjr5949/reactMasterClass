9a5ca9c42592d45575824c1d4e1373ec3f93f921
alstjr5949 <alstjr5949@gmail.com>
2022-02-09 19:54:35 +0900

    slider next btn 7.12

--- src/App.tsx
+++ src/App.tsx
@@ -68,62 +68,74 @@ const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #e09, #d0e);
-  button {
-    position: absolute;
-    bottom: 300px;
-  }
+  flex-direction: column;
 `;
 
-const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  margin-bottom: 20px;
-`;
-
-const boxVariants = {
-  start: {
+const box = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 1,
+    },
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 50,
+    transition: {
+      duration: 0.5,
+    },
   },
 };
 
+const Box = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 200px;
+  position: absolute;
+  top: 100px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  margin-bottom: 20px;
+  font-size: 28px;
+`;
+
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
   return (
     <>
       <GlobalStyle />
       <Wrapper>
         <AnimatePresence>
-          {showing ? (
-            <Box
-              variants={boxVariants}
-              initial="start"
-              animate="visible"
-              exit="leaving"
-            />
-          ) : null}
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+            i === visible ? (
+              <Box
+                variants={box}
+                initial="invisible"
+                animate="visible"
+                exit="exit"
+                key={i}
+              >
+                {i}
+              </Box>
+            ) : null
+          )}
         </AnimatePresence>
-        <button onClick={toggleShowing}>Click</button>
+        <button onClick={nextPlease}>next</button>
       </Wrapper>
     </>
   );
