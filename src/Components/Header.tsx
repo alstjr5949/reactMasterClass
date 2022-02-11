1d84640b67b1755d541771998e88ac369d9e48e1
alstjr5949 <alstjr5949@gmail.com>
2022-02-11 23:10:26 +0900

    header part 1 ~8.1

---
+++ src/Components/Header.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  background-color: red;
+  font-size: 12px;
+`;
+
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.svg`
+  margin-right: 50px;
+`;
+
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  margin-right: 20px;
+`;
+
+function Header() {
+  return (
+    <Nav>
+      <Col>
+        <Logo />
+        <Items>
+          <Item>Home</Item>
+          <Item>TV Show</Item>
+        </Items>
+      </Col>
+      <Col>
+        <button>Search</button>
+      </Col>
+    </Nav>
+  );
+}
+
+export default Header;
